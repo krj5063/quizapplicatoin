0c69fa05807907754bef93514e897a1c8f9086f2
krj5063 <krj5063@gmail.com>
2015-01-02 10:41:45 -0500

    new app

--- js/app.js
+++ js/app.js
@@ -1,41 +1,47 @@
-var pos = 0;
-var q1 = {"question":"Do you like the color blue?", "answer":"Yooo1", "chA":"A1", "chB":"B1", "chC":"C1"}
-var q2 = {"question":"Do you like the color red?", "answer":"Yooo2", "chA":"A2", "chB":"B2", "chC":"C2"}
-var q3 = {"question":"Do you like the color green?", "answer":"Yooo3", "chA":"A3", "chB":"B3", "chC":"C3"}
-var q4 = {"question":"Do you like the color yellow?", "answer":"Yooo4", "chA":"A4", "chB":"B4", "chC":"C4"}
-var qBank = [q1,q2,q3,q4];
-var correctAnswer;
-var correctResponses = 0;
-
-var askQ = function () {
-    $("#test").html(qBank[0]["question"]);
-};
-
-
+$(function(){
+    var current = -1,
+        questions = [{question: 'What two elements make up water?', 
+                      correct: 3, 
+                      choices: ['Hydrogen and Iron', 'Iron and Fat', 'Hydrogen and Oxygen', 'Oxygen and Nitrogen']},
+                     {question: 'What percentage of the earth is covered by water?', 
+                      correct: 1, 
+                      choices: ['70%', '30%', '55%', '90%']},
+                     {question: 'Which of the following is not a state of water?', 
+                      correct: 4, 
+                      choices: ['Liquid', 'Gas', 'Solid', 'Fluid']},
+                     {question: 'Water in a gas state is known as...', 
+                      correct: 2, 
+                      choices: ['Ice', 'Steam', 'Mold', 'Salt']}];
     
-
-$(document).ready(function(){
-
-askLoop = function() {
-    for(i=0; i<qBank.length; i++) { 
-        $("#test").html(qBank[i]["question"]+"<br><br>"+qBank[i]["answer"]+"<br><br>"+qBank[i]["chA"]+"<br><br>"+qBank[i]["chB"]+"<br><br>"+qBank[i]["chC"]);
+    function askLoop() {
+        var $test = $('#test'),i=0;
+        current++;
+        $test.empty();
+        $test.append($('<p/>').html(questions[current].question+"<br>"));
+        var $quizBottom = $('#quizBottom');
+        $quizBottom.empty();
+        $quizBottom.append($('<p/>').html("Question "+(current+1)+"/4"));
+        
+        for(i=0;i < questions[current].choices.length; i++){
+            $test.append($('<input/>')
+                         .attr('type', 'radio')
+                         .attr('name', 'answer')
+                         .val(questions[current].choices[i]),
+                         $('<label/>').html(questions[current].choices[i]+"<br><br>"));
+        
+        }
     }
     
-};
-
+    $('#submit').on('click', askLoop)
+    .on('click', function(){$(this).html('Submit')});
+    
+    });
     
 
     
-});
 
 
-//var askQuestion = function() {
-//$(".test").innerHTML = questions[0]+"<br>"+choicesA[0]+"<br>"+choicesB[0]+"<br>"+choicesC[0];
-//};    
-    
-    
-//askQuestion();
-   
+
     
 
 
